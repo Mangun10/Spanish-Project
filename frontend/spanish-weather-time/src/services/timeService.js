3184e80dee9d147efcfe8375c3215271d1244f3e
Mangun10 <145484910+Mangun10@users.noreply.github.com>
2025-04-13 16:24:33 +0530

    removed hahaha

--- frontend/spanish-weather-time/src/services/timeService.js
+++ frontend/spanish-weather-time/src/services/timeService.js
@@ -309,7 +309,7 @@ export const getTimeOfDayInSpanish = (datetime) => {
   if (hours >= 6 && hours < 12) {
     return 'de la mañana';
   } else if (hours >= 12 && hours < 21) {
-    return 'de la hahaha tarde';
+    return 'de la tarde';
   } else {
     return 'de la noche';
   }
